93aee057f4333fc49e29fdf4a7850c3ef078c211
Seraphin Haeuptli <seraphin@setsail.ch>
2026-01-18 23:17:08 +0100

    Fix the fix

--- app.vue
+++ app.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="app-wrapper">
-    <MobileMenu/>
-    <AppHeader/>
-    <NuxtPage data-aos="fade-in"/>
-    <AppFooter/>
-  </div>
+  <MobileMenu/>
+  <AppHeader/>
+  <NuxtPage data-aos="fade-in"/>
+  <AppFooter/>
 </template>
 
 <script>
 import AOS from 'aos';
-import 'aos/dist/aos.css';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 export default {
+  /**
+   * * Watch for route changes
+   * This event is triggered when the route changes.
+   * @param {Object} to - Route object
+   * @param {Object} from - Route object
+   */
   watch: {
     $route(to, from) {
       console.log('De', from.fullPath, 'a', to.fullPath);
@@ -18,36 +22,25 @@ export default {
   },
   mounted() {
     AOS.init({
-        disable: false,
-        startEvent: 'DOMContentLoaded',
-        initClassName: 'aos-init',
-        animatedClassName: 'aos-animate',
-        useClassNames: false,
-        disableMutationObserver: false,
-        debounceDelay: 50,
-        throttleDelay: 99,
-        offset: 120,
-        delay: 0,
-        duration: 400,
-        easing: 'ease',
-        once: false,
-        mirror: false,
-        anchorPlacement: 'top-bottom',
+        // Global settings:
+        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        initClassName: 'aos-init', // class applied after initialization
+        animatedClassName: 'aos-animate', // class applied on animation
+        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+        
+        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+        offset: 120, // offset (in px) from the original trigger point
+        delay: 0, // values from 0 to 3000, with step 50ms
+        duration: 400, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
     });
   }
 }
 </script>
-
-<style>
-.app-wrapper {
-  min-height: 100vh;
-  min-height: -webkit-fill-available;
-}
-
-html, body {
-  height: 100%;
-  min-height: 100vh;
-  margin: 0;
-  padding: 0;
-}
-</style>
